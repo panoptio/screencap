6bafb012aad68846cef20c0f3e05763db10644f9
Mike McMurray <mmcmurray@panopt.io>
2018-08-15 14:43:00 -0500

    converting tabs to spaces

--- screenshot.js
+++ screenshot.js
@@ -9,32 +9,32 @@ const BUTTON_SELECTOR = '#login_submit';
 var base_uri = 'https://insights.newrelic.com/accounts/' + CREDS.accountid + '/dashboards/'
 
 var dashboards = {
-		"661669" : "esclient_singapore_legacy",
-		"661648" : "bridge_singapore_apac",
-		"661653" : "bridge_oregon_rainbow",
-	}
+    "661669" : "esclient_singapore_legacy",
+    "661648" : "bridge_singapore_apac",
+    "661653" : "bridge_oregon_rainbow",
+    }
 
 async function run() {
     const browser = await puppeteer.launch({
-		  headless: false
-		});
+        headless: false
+        });
     const page = await browser.newPage();
-    
+
     await page.setViewport({ width: 1920, height: 1200});  
     await page.goto(login_page);
     await page.click(USERNAME_SELECTOR);
-	await page.keyboard.type(CREDS.username);
+    await page.keyboard.type(CREDS.username);
 
-	await page.click(PASSWORD_SELECTOR);
-	await page.keyboard.type(CREDS.password);
+    await page.click(PASSWORD_SELECTOR);
+    await page.keyboard.type(CREDS.password);
 
-	await page.click(BUTTON_SELECTOR);
+    await page.click(BUTTON_SELECTOR);
 
-	for(var dashboard in dashboards) {
-		await page.waitFor(1*1000);
-		await page.goto(base_uri + dashboard + '?' + extra_params);
-		await page.waitFor(5*1000);
-	    await page.screenshot({ path: 'screenshots/' + dashboards[dashboard] + '.png' });
+    for(var dashboard in dashboards) {
+        await page.waitFor(1*1000);
+        await page.goto(base_uri + dashboard + '?' + extra_params);
+        await page.waitFor(5*1000);
+        await page.screenshot({ path: 'screenshots/' + dashboards[dashboard] + '.png' });
     }
 
     browser.close();
